3532044fd58e6e2f8d9d26db372d1a7b55b84bf0
cocoa-dev-1 <sinmounbu0510@naver.com>
2023-04-22 11:40:18 +0900

    fix: fix notice list error

--- extensions/notice.py
+++ extensions/notice.py
@@ -52,7 +52,9 @@ class Notice(commands.Cog):
 
             embed = discord.Embed(title="공지 목록")
             for d in data:
-                embed.add_field(name=f"{d[2]}(`{d[0]}`)", value=f"생성 시각: <t:{int(int(d[4])/1000)}>\n카테고리: {d[1]}", inline=False)
+                embed.add_field(name=f"{d[2]}(`{d[0]}`)",
+                                value=f"생성 시간: <t:{int(int(d[5]) / 1000)}>\n노출 시작 시간: <t:{int(int(d[6]) / 1000)}>\n노출 종료 시간: <t:{int(int(d[7]) / 1000)}>\n카테고리: {d[1]}",
+                                inline=False)
             return await ctx.respond(embed=embed)
 
         return await ctx.respond("권한이 없습니다.")
